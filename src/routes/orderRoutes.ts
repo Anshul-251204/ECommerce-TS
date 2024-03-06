12761f2e018c9299a1b618219794d22dd286a36b
Anshul  choure <139856005+Anshul-251204@users.noreply.github.com>
2024-03-06 23:39:23 +0530

    add seller middleware:

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { myOrder, newOrder } from "../controllers/orderController.js";
 import auth from "../middlewares/authMiddleware.js";
+import seller from "../middlewares/sellerMiddlerware.js";
 
 const router = Router();
 
 router.route("/").post(auth, newOrder);
 router.route("/my-order").get(auth,myOrder);
+// router.route("/all").get(seller,)
 
 
 export default router;
